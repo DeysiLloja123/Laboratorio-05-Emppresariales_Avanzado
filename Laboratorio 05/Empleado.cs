c3c695ed9d78cddfada9e41c430a221691952d74
Alumno <Alumno@LAB1504-26>
2024-04-13 16:06:49 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio 05/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_05
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public string Tratamiento { get; set; }
+
+        // Constructor de la clase Empleado
+        public Empleado(int idEmpleado, string apellidos, string nombre, string cargo, string tratamiento)
+        {
+            IdEmpleado = idEmpleado;
+            Apellidos = apellidos;
+            Nombre = nombre;
+            Cargo = cargo;
+            Tratamiento = tratamiento;
+        }
+    }
+}
